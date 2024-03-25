769794902af0bf2e5ebc7e848a694a728fd8c2ba
Zafarov Polat <atuin59354081@gmail.com>
2024-03-25 15:37:47 +0500

    Some style fixed

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         {data.data.map((d, i) => {
           return <Route path={`/card/${i + 1}`} element={
             <>
-              <div className="w-full lg:w-4/12 px-4 mx-auto">
+              <div className="w-full px-4 mx-auto">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
                   <div className="px-6">
                     <div className="flex flex-wrap justify-center">
@@ -63,7 +63,7 @@ function App() {
                       </div>
                     </div>
                     <div className="text-center mt-12">
-                      <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                      <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700">
                         {d.name}
                       </h3>
                       <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
