ff9f9ffb91b01ddbf92e1bf55c81361abe04f93f
Zafarov Polat <atuin59354081@gmail.com>
2024-03-25 15:28:58 +0500

    Routes and cards done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,104 @@
+import { useQuery } from "react-query";
+import axios from "axios";
 import { useState } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import { Login } from "./components/Login";
+import { Dashboard } from "./components/Dashboard";
+import "./App.css";
+
+
+const fetchData = () => {
+  return axios.get("https://65e83bc64bb72f0a9c4eac3a.mockapi.io/fakeAPI");
+};
+
 
 function App() {
+
+  const { isLoading, data, isError, isFetching } = useQuery("data", fetchData);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        {data.data.map((d, i) => {
+
+          return <Route path={`/card/${i + 1}`} element={
+            <>
+              <div className="w-full lg:w-4/12 px-4 mx-auto">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+                  <div className="px-6">
+                    <div className="flex flex-wrap justify-center">
+                      <div className="w-full px-4 flex justify-center">
+                        <div className="">
+                          <img alt="..." src={d.img} className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px" />
+                        </div>
+                      </div>
+                      <div className="w-full px-4 text-center mt-20">
+                        <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                          <div className="mr-4 p-3 text-center">
+                            <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                              22
+                            </span>
+                            <span className="text-sm text-blueGray-400">Friends</span>
+                          </div>
+                          <div className="mr-4 p-3 text-center">
+                            <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                              10
+                            </span>
+                            <span className="text-sm text-blueGray-400">Photos</span>
+                          </div>
+                          <div className="lg:mr-4 p-3 text-center">
+                            <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                              89
+                            </span>
+                            <span className="text-sm text-blueGray-400">Comments</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-center mt-12">
+                      <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                        {d.name}
+                      </h3>
+                      <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                        <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+                        Los Angeles, California
+                      </div>
+                      <div className="mb-2 text-blueGray-600 mt-10">
+                        <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
+                        Solution Manager - Creative Tim Officer
+                      </div>
+                      <div className="mb-2 text-blueGray-600">
+                        <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
+                        University of Computer Science
+                      </div>
+                    </div>
+                    <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                      <div className="flex flex-wrap justify-center">
+                        <div className="w-full lg:w-9/12 px-4">
+                          <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                            An artist of considerable range, {`${d.name.split(" ")[0]}`} the name taken
+                            by Melbourne-raised, Brooklyn-based Nick Murphy
+                            writes, performs and records all of his own music,
+                            giving it a warm, intimate feel with a solid groove
+                            structure. An artist of considerable range.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          } />
+        })}
       </Routes>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,8 @@
 import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
-import { Header } from "./sections/Header";
-import { Footer } from "./sections/Footer";
-import { Hero } from "./sections/Hero";
-import { Work } from "./sections/Work";
-import { Review } from "./sections/Review";
-import { Latest } from "./sections/Latest";
-import { Contact } from "./sections/Contact";
+import { Link } from "react-router-dom";
+
 
 const fetchData = () => {
   return axios.get("https://65e83bc64bb72f0a9c4eac3a.mockapi.io/fakeAPI");
@@ -21,32 +16,39 @@ const Home = () => {
   }
 
   return (
-    // <div className="flex flex-wrap justify-center gap-5 items-center">
-    //   {data.data.map((d) => (
-    //     <div className="card">
-    //       <img src={d.img} alt="image" />
-    //       <h1>{d.name}</h1>
-    //     </div>
-    //   ))}
-    // </div>
     <>
-      <Header></Header>
 
-      <main>
+      <nav className="w-100 flex justify-between border-b-2 p-3">
+        <a href="" className="text-xl">Cards</a>
+        <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to={"/login"}>Login</Link>
+      </nav>
 
-        <Hero></Hero>
+      <div className="grid grid-cols-4 justify-center gap-5 items-center p-10">
+        {data.data.map((d, i) => {
+          return <div>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <Link to={`/card/${i + 1}`} href="#">
+                <img className="rounded-t-lg w-full" src={d.img} alt="" />
+              </Link>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{d.name}</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  In the mask of time, John Doe hides his trace,
+                  Deep mysteries in his heart, unknown threads of light blaze.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Read more
+                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
 
-        <Work></Work>
-
-        <Review></Review>
-
-        <Latest></Latest>
-
-        <Contact></Contact>
-
-      </main>
-
-      <Footer></Footer>
+        })}
+      </div >
     </>
   );
 };
