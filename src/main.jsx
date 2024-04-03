55ad10fd8b9acb5e4b09e105bd6a0917cd592a1f
lobzy007 <ahrorkhidirov@gmail.com>
2024-04-03 17:17:29 +0500

    internalisation

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
-
+import "./i18n/i18n.js";
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
